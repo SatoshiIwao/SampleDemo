a7434715880610c797e7fc7ced2cc8663b7e539b
Satoshi Iwao <nirnirva.s.t.s1551@gmail.com>
2015-01-09 16:00:34 -0800

    add simple change

--- TutorialGitHub/src/Tutorial.java
+++ TutorialGitHub/src/Tutorial.java
@@ -5,6 +5,10 @@ public class Tutorial
 	public static void main(String[] args) 
 	{
 		System.out.println("hello world.");
+		
+		int number = 23;
+		
+		System.out.printf("my age is %d.", number);
 	}
 
 }
